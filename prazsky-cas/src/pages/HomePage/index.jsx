6123bed7c33ebd295818b03c02268deaa3d4024e
Ester <kocabova@mamiart.cz>
2024-03-03 22:12:16 +0100

    druhá část

--- prazsky-cas/src/pages/HomePage/index.jsx
+++ prazsky-cas/src/pages/HomePage/index.jsx
@@ -4,16 +4,17 @@ import './style.css';
 export const HomePage = () => {
   // useEffect(() => {alert("Ahoj Týno :)")}, [])
   const [dateTime, setDateTime] = useState('')
+  const [timezone, setTimezone] = useState('Europe/Prague')
 
   useEffect( () => {
     const fetchDate = async () => {
-      const response = await fetch('https://worldtimeapi.org/api/timezone/Europe/Prague');
+      const response = await fetch(`https://worldtimeapi.org/api/timezone/${timezone}`);
       const data = await response.json();
       setDateTime(data.datetime)
       };
       fetchDate()
   }
- , [])
+ , [timezone])
 
   return (
     <div className="container">
@@ -23,8 +24,17 @@ export const HomePage = () => {
       </header>
       <main>
         <p>
-        {dateTime}
+        Hodnota datetime: {dateTime}
         </p>
+        <span>Vyberte zónu: </span>
+        <select onChange={(e) => setTimezone(e.target.value)} value={timezone}>
+          <option value="America/New_York">New York</option>
+          <option value="Europe/London">Londýn</option>
+          <option value="Europe/Moscow">Moskva</option>
+          <option value="Europe/Prague">Praha</option>
+          <option value="Asia/Hong_Kong">Hong Kong</option>
+          <option value="Asia/Jerusalem">Jeruzalém</option>
+        </select>
       </main>
       <footer>
         <p></p>
